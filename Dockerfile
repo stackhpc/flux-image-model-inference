a94192ad7095af82032d2399f18c2e784f4d207e
sd109 <sdavidson327@gmail.com>
2024-11-07 16:16:09 +0000

    Add Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM python:3.10
+
+# https://stackoverflow.com/questions/55313610/importerror-libgl-so-1-cannot-open-shared-object-file-no-such-file-or-directo
+RUN apt-get update && apt-get install ffmpeg libsm6 libxext6  -y
+
+WORKDIR /app
+
+COPY src src
+COPY setup.py setup.py
+COPY pyproject.toml pyproject.toml
+RUN pip install ".[gradio]"
+
+COPY model_licenses model_licenses
+COPY LICENSE LICENSE
+
+# Copy this last for quicker image builds
+# when only modifying the Gradio UI
+COPY demo_gr.py demo_gr.py
+
+ENTRYPOINT ["python", "demo_gr.py", "--name", "flux-schnell"]
---
+++ build-and-push-image.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+if [[ $(git diff --staged) ]]; then
+    echo "Repo checkout has staged changes - please comment them and try again."
+    exit 1
+fi
+
+# if [[ ! $(git diff-files --quiet) ]]; then
+#     echo "Repo checkout has uncommitted changes - please commit or .gitignore them and try again."
+#     exit 1
+# fi
+
+if [[ $(git ls-files --exclude-standard --others) ]]; then
+    echo "Repo checkout has untracked files - please commit or .gitignore them and try again."
+    exit 1
+fi
+
+PACKAGE_REPO=ghcr.io/stackhpc/flux-image-model-inference
+COMMIT_SHA=$(git rev-parse --short HEAD)
+IMAGE_NAME=$PACKAGE_REPO:$COMMIT_SHA
+
+docker build --platform linux/amd64 . -t $IMAGE_NAME
+docker push $IMAGE_NAME
